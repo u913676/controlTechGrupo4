ffe324045a373197aa0aa89ba05894acad32ee6e
Ro Martinez <romartinez@Ros-MacBook-Pro.local>
2026-07-15 11:34:36 -0500

    delete folders

--- nodeJSClient/server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import dotenv from "dotenv";
-import itassetssRouter from "./routers/itasset.routers.mjs";
-
-const app = express();
-app.use(express.json());
-app.use("/", itassetssRouter);
-app.listen(3000, () => console.log("Servidor local en http://localhost:3000"));
